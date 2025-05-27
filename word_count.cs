41b57c047ba2c6c7ec28caa1eaee261f9f9c8564
Rokaiya-Akter <rokaiyaakter185@gmail.com>
2025-05-27 22:59:27 +0300

    Add files via upload

---
+++ word_count.cs
@@ -0,0 +1,22 @@
+public static Dictionary<string, int> CountWords(string content)
+{
+    var wordCounts = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+    // Split content into words using common separators
+    var words = content.Split(new[] { ' ', '\n', '\r', '\t', '.', ',', ';', '!', '?', ':', '-', '_', '\"', '\'', '(', ')' }, 
+                              StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var word in words)
+    {
+        var cleanWord = word.Trim().ToLower();
+        if (!string.IsNullOrWhiteSpace(cleanWord))
+        {
+            if (!wordCounts.ContainsKey(cleanWord))
+                wordCounts[cleanWord] = 0;
+
+            wordCounts[cleanWord]++;
+        }
+    }
+
+    return wordCounts;
+}
