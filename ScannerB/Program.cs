0c44c60ff855dc146a60aebe167abc0a9b35185f
Rokaiya-Akter <rokaiyaakter185@gmail.com>
2025-05-30 09:49:36 +0300

    Update Program.cs

--- ScannerB/Program.cs
+++ ScannerB/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic; 
-//starting class
+//starting class sec
 class ScannerB
 {
     //using static void function
