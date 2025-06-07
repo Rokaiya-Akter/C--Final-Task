346381b322d1d6107bc2bb30c5b16f2f41691774
Rokaiya-Akter <rokaiyaakter185@gmail.com>
2025-06-08 02:32:52 +0300

    Update word_count.cs

--- word_count.cs
+++ word_count.cs
@@ -5,7 +5,7 @@ public static Dictionary<string, int> CountWords(string content)
     // Split content into words using common separators
     var words = content.Split(new[] { ' ', '\n', '\r', '\t', '.', ',', ';', '!', '?', ':', '-', '_', '\"', '\'', '(', ')' }, 
                               StringSplitOptions.RemoveEmptyEntries);
-//starting foreach sec
+
     foreach (var word in words)
     {
         var cleanWord = word.Trim().ToLower();
